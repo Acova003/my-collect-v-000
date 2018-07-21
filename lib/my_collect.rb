ae705de89281f0923f923a8c13a44221a18074e7
Amee Covarrubias <ameecova@yahoo.com>
2018-07-21 09:06:06 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,11 @@
 def my_collect(collection)
   i = 0
   new_arr = []
-  my_collect(collection) do |el|
-    new_arr << yield(collection[i])
-    i += 1
+  while i < collection.length
+    my_collect(collection) do |el|
+      new_arr << yield(collection[i])
+      i += 1
+    end
   end
   new_arr
 end 
